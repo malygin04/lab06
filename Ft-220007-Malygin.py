2c3274a67a8c38f8952518e3313ac5356900f113
malygin04 <102820638+malygin04@users.noreply.github.com>
2023-11-12 20:56:32 +0500

    Update Ft-220007-Malygin.py
    изменил код: сделал так чтобы во входных данных присутствовали данные попарного сравнения критериев

--- Ft-220007-Malygin.py
+++ Ft-220007-Malygin.py
@@ -3,40 +3,37 @@ def get_criteria_weights(num_criteria):
     # Функция для получения матрицы весов критериев от пользователя
     # Принимает количество критериев в качестве аргумента
     criteria_weights = []
-
     for i in range(num_criteria):
         row = []
         for j in range(num_criteria):
-            while True:
-                try:
-                    weight = float(input(f"Введите вес для критерия {i + 1} по отношению к критерию {j + 1}: "))
-                    break
-                except ValueError:
-                    print("Ошибка ввода! Введите число.")
-
-            row.append(weight)
-
+            if i == j:
+                row.append(1.0)
+            elif i < j:
+                while True:
+                    try:
+                        weight = float(input(f"Введите вес для критерия {i + 1} по отношению к критерию {j + 1}: "))
+                        break
+                    except ValueError:
+                        print("Ошибка ввода! Введите число.")
+                row.append(weight)
+            else:
+                row.append(1.0 / criteria_weights[j][i])
         criteria_weights.append(row)
-
     return criteria_weights
-
+            
 
 def calculate_weights(criteria_weights):
     # Функция для вычисления весовых коэффициентов на основе матрицы весов
     # Принимает матрицу весов в качестве аргумента
     num_criteria = len(criteria_weights)
     weights = []
-
     for i in range(num_criteria):
         product = 1
         for j in range(num_criteria):
             product *= criteria_weights[i][j] / sum(criteria_weights[j])
-
         weights.append(product)
-
     total_weight = sum(weights)
     weights = [weight / total_weight for weight in weights]
-
     return weights
 
 
@@ -48,10 +45,8 @@ def main():
             break
         except ValueError:
             print("Ошибка ввода! Введите целое число.")
-
     criteria_weights = get_criteria_weights(num_criteria)
     weights = calculate_weights(criteria_weights)
-
     print("Весовые коэффициенты:")
     for i, weight in enumerate(weights):
         print(f"Критерий {i + 1}: {weight:.2f}")
